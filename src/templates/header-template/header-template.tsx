d4a974a423c4d79367fbd512b5dc9f2175a5a3cd
Rony Antolinez <rony1710.dev@gmail.com>
2025-02-18 23:45:12 -0500

    wip: add react router dom and custom fonts for project

--- src/templates/header-template/header-template.tsx
+++ src/templates/header-template/header-template.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Images } from "../../assets/media";
 import styles from "./header-template.module.css";
 
@@ -5,6 +6,24 @@ export const HeaderTemplate = () => {
   return (
     <header className={styles["header-main-wrapper"]}>
       <img src={Images.Logo} alt="technologycal logo" />
+
+      <nav className={styles["header-navbar-wrapper"]}>
+        <Link className={styles["header-navbar-link"]} to="/">
+          Home
+        </Link>
+        <Link className={styles["header-navbar-link"]} to="/features">
+          Features
+        </Link>
+        <Link className={styles["header-navbar-link"]} to="/resources">
+          Resources
+        </Link>
+        <Link className={styles["header-navbar-link"]} to="/pricing">
+          Pricing
+        </Link>
+        <Link className={styles["header-navbar-link"]} to="/blog">
+          Blog
+        </Link>
+      </nav>
     </header>
   );
 };
