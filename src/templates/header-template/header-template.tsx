bcdc93aa907017cd7e7c895e32cc3f5c276916c8
Rony Antolinez <rony1710.dev@gmail.com>
2025-02-18 23:26:10 -0500

    wip: add header template module css

--- src/templates/header-template/header-template.tsx
+++ src/templates/header-template/header-template.tsx
@@ -1,8 +1,9 @@
 import { Images } from "../../assets/media";
+import styles from "./header-template.module.css";
 
 export const HeaderTemplate = () => {
   return (
-    <header>
+    <header className={styles["header-main-wrapper"]}>
       <img src={Images.Logo} alt="technologycal logo" />
     </header>
   );
